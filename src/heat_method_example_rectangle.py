af8c0726036493e06fe89b6cebb6958cc559b15c
Stephen Jasina <stephenjasina@gmail.com>
2022-08-15 20:01:20 -0400

    Update heat method rectangle example to demonstrate boundary more

--- src/heat_method_example_rectangle.py
+++ src/heat_method_example_rectangle.py
@@ -10,7 +10,7 @@ M = RectangleMesh(width, height)
 laplacian_forward = laplacian.Forward(M)
 geodesic_forward = geodesic.Forward(M, laplacian_forward)
 
-geodesic_forward.calc(M.nearest_vertex_index(0.3, 0.4))
+geodesic_forward.calc(M.nearest_vertex_index(0., 0.))
 phi = geodesic_forward.phi
 
 rng = np.random.default_rng()
@@ -19,4 +19,4 @@ for _ in range(10):
     y = rng.random()
     print(f'Estimated: {phi[M.nearest_vertex_index(x, y)]:.6f};',
         end=' ')
-    print(f'True: {np.sqrt((x - 0.3)**2 + (y - 0.4)**2):.6f}')
+    print(f'True: {np.sqrt(x**2 + y**2):.6f}')
