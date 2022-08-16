Fix Wolfe step computation

src/linear_geodesic_optimization/data/phony.py
@@ -14,8 +14,8 @@ def sphere_true(mesh):
     ]
     directions = [SphereMesh.latitude_longitude_to_direction(lat, long)
                   for (lat, long) in lat_long_pairs]
-    s_indices = [mesh.nearest_direction_index(direction)
-                 for direction in directions]
+    s_indices = {mesh.nearest_direction_index(direction)
+                 for direction in directions}
     ts = {si: [(sj, np.arccos(dsi @ dsj))
                for j, (sj, dsj) in enumerate(zip(s_indices, directions))
                if (i - j) % 6 != 0]
@@ -27,7 +27,7 @@ def sphere_random(mesh, count=10, connectivity=5):
     rng = np.random.default_rng(0)
 
     v = mesh.get_vertices()
-    s_indices = rng.choice(range(v.shape[0]), count, replace=False)
+    s_indices = set(rng.choice(range(v.shape[0]), count, replace=False))
     connections = set()
     for si in s_indices:
         distances = sorted([(np.arccos(np.clip(v[si] @ v[sj]
@@ -57,4 +57,4 @@ def rectangle_simplex(mesh):
         (1., 1.),
     ])
     ts = {si: [(sj, np.random.normal(1., 0.1)) for sj in s_indices if si != sj] for si in s_indices}
-    return s_indices, ts
+    return set(s_indices), ts

src/optimization_rectangle.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-from linear_geodesic_optimization.data import phony, measured
+from linear_geodesic_optimization.data import measured
 from linear_geodesic_optimization.mesh.rectangle import Mesh as RectangleMesh
 from linear_geodesic_optimization.optimization import optimization, standard
 from linear_geodesic_optimization.plot import get_scatter_fig, Animation3D
 
 # Construct the mesh
-width = 8
-height = 8
+width = 14
+height = 14
 mesh = RectangleMesh(width, height)
 partials = mesh.get_partials()
 V = partials.shape[0]
-z = mesh.set_parameters(np.random.normal(0., 0.1, width * height))
+z = mesh.set_parameters(np.random.normal(0., 1., width * height))
 
 dif_v = {l: partials[l] for l in range(V)}
 
@@ -40,7 +40,7 @@ max_iterations = 10
 
 get_scatter_fig(hierarchy).show()
 
-standard.steepest_descent(z, mesh.set_parameters, f, g, max_iterations, diagnostics)
+standard.lbfgs(z, mesh.set_parameters, f, g, max_iterations, diagnostics)
 
 get_scatter_fig(hierarchy).show()
 animation_3D.get_fig(duration=50).show()