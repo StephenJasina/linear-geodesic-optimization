23c0c12d4b4d35f47fafc5d14184a11cf7339c0f
Stephen Jasina <stephenjasina@gmail.com>
2022-10-16 22:42:42 -0400

    Add a basic mesh that is just a container for data

---
+++ src/linear_geodesic_optimization/mesh/basic_mesh.py
@@ -0,0 +1,23 @@
+class Mesh:
+    def __init__(self, vertices, edges, faces,
+                 boundary_vertices=set(), boundary_edges=set()):
+        self.vertices = vertices
+        self.edges = edges
+        self.faces = faces
+        self.boundary_vertices = boundary_vertices
+        self.boundary_edges = boundary_edges
+
+    def get_vertices(self):
+        return self.vertices
+
+    def get_edges(self):
+        return self.edges
+
+    def get_faces(self):
+        return self.faces
+
+    def get_boundary_vertices(self):
+        return self.boundary_vertices
+
+    def get_boundary_edges(self):
+        return self.boundary_edges
