Fix regression gradient computation

src/linear_geodesic_optimization/optimization/linear_regression.py
@@ -11,8 +11,6 @@ class Forward:
         self.residuals = None
         self.lse = None
 
-        self.beta = None
-
     def calc(self, phi, t):
         E = phi.shape[0]
 
@@ -31,36 +29,17 @@ class Forward:
         self.d = self.d_tilde / (self.d_tilde @ self.d_tilde / E)**0.5
         self.residuals = t - np.sum(t) / E - (self.d @ t / E) * self.d
         self.lse = self.residuals @ self.residuals / (t.shape[0] * t @ t)
-        self.beta = None
 
     def get_beta(self, phi, t):
-        E = phi.shape[0]
-
-        # If phi and t haven't changed, don't do any additional work.
-        if (self._phi is not None and self._phi.shape == phi.shape
-            and np.allclose(self._phi, phi)
-            and self._t is not None and self._t.shape == t.shape
-            and np.allclose(self._t, t)
-            and self.beta is not None):
-            return self.beta
-
-        self._phi = np.copy(phi)
-        self._t = np.copy(t)
-
         # Note that the following disagrees with the notation in the writeup
         # In particular, beta here is the coefficients when relating phi to t
         # (as opposed to relating d to t).
+        E = phi.shape[0]
         denominator = E * (phi @ phi) - np.sum(phi)**2
-        self.beta = (
+        return (
             (phi @ phi * np.sum(t) - np.sum(phi) * (phi @ t)) / denominator,
             (E * (phi @ t) - np.sum(phi) * np.sum(t)) / denominator,
         )
-        self.d_tilde = None
-        self.d = None
-        self.residuals = None
-        self.lse = None
-
-        return self.beta
 
 class Reverse:
     def __init__(self, linear_regression_forward=None):
@@ -113,7 +92,7 @@ class Reverse:
         self.dif_d_tilde = self._dif_phi - np.sum(self._dif_phi) / E
         self.dif_d = (self.dif_d_tilde
                       - (self._d @ self.dif_d_tilde)
-                        * self._d) / linalg.norm(self._d_tilde)
+                        * self._d / E) / (linalg.norm(self._d_tilde) / E**0.5)
         self.dif_residuals = -(self._d @ self._t * self.dif_d
                                + self.dif_d @ self._t * self._d) / E
         self.dif_lse = 2 * self._residuals @ self.dif_residuals / (t.shape[0] * t @ t)