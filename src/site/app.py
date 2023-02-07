Fix scaling on floating graph

src/site/app.py
@@ -21,7 +21,7 @@ from python.geodesic import GeodesicDistanceComputation
 sys.path.append('..')
 from linear_geodesic_optimization.mesh.rectangle import Mesh as RectangleMesh
 
-mesh_path = '/home/jasina/Desktop/out/0_1_1'
+mesh_path = '/home/jasina/Desktop/1203'
 
 sys.path.append(r'python/surface/src')
 
@@ -117,8 +117,8 @@ def get_z_from_path(path):
         x = list(sorted(set(vertices[:,0])))
         y = list(sorted(set(vertices[:,1])))
         z = vertices[:,2]
-        z = z - np.mean(z)
-        z = z / (max(z) - min(z))
+        z = z - np.amax(z)
+        z = z / -np.amin(z)
         return z.tolist()
 
 @app.route('/calc-surface', methods=['POST'])