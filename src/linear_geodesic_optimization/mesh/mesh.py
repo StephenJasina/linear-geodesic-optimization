1fdb3350c5c34ae1bef5044df1e5fda684f1eb3f
Stephen Jasina <stephenjasina@gmail.com>
2023-05-15 04:27:23 -0500

    Improve code documenation

--- src/linear_geodesic_optimization/mesh/mesh.py
+++ src/linear_geodesic_optimization/mesh/mesh.py
@@ -1,70 +1,92 @@
+"""Module containing an interface for a mesh representing a manifold."""
+
+import typing
+
+import dcelmesh
+import numpy as np
+
+
 class Mesh:
-    def get_topology(self):
-        '''
-        Return the topology of the mesh.
+    """
+    A triangular mesh representing a manifold.
 
-        This should be a `dcelmesh.Mesh` object.
-        '''
+    The mesh can be decomposed into its underlying topology and the
+    coordinates of its vertices. Furthermore, each vertex can be thought
+    of as being determined by a single parameter.
+    """
 
-    def get_partials(self):
-        '''
-        We assume the mesh is parameterized by |V| scalars, each of which
-        affects exactly one vertex. This function returns the non-zero partial
-        derivatives.
+    def get_topology(self) -> dcelmesh.Mesh:
+        """Return the topology of the mesh."""
+        pass
 
-        For efficiency, this returns a |V| by 3 array. The output of this
-        function should be treated as "read only."
-        '''
-
-        raise NotImplementedError
-
-    def get_coordinates(self):
-        '''
+    def get_coordinates(self) -> np.ndarray:
+        """
         Return the coordinates of the vertices of the mesh.
 
         For efficiency, this returns a |V| by 3 array.
-        '''
-
+        """
         raise NotImplementedError
 
-    def get_parameters(self):
-        '''
-        Return the parameters of the vertices of this mesh, ordered in the same
-        way as the output of `get_vertices`.
-        '''
+    def get_parameters(self) -> np.ndarray:
+        """
+        Return the parameters of the vertices of this mesh.
 
+        The output is ordered in the same way as the output of
+        `Mesh.get_coordinates`.
+        """
         raise NotImplementedError
 
-    def set_parameters(self, parameters):
-        '''
-        Set the parameters of the vertices of this mesh, ordered in the same
-        way as the output of `get_vertices`.
-        '''
+    def set_parameters(self, parameters: np.ndarray) -> np.ndarray:
+        """
+        Set the parameters of the vertices of this mesh.
 
+        The input to this function should be ordered in the same way as
+        the output of `Mesh.get_coordinates`. This function returns the
+        resulting parameters, which should be treated as read only.
+        """
         raise NotImplementedError
 
-    def updates(self):
-        '''
-        Return the number of times `set_rho` has been called. This function is
-        an easy (O(1)) way to determine whether the mesh has been updated.
-        '''
+    def updates(self) -> int:
+        """
+        Return the number of calls to `Mesh.set_parameters`.
 
+        This function is an easy (O(1)) way to determine whether the
+        mesh has been updated.
+        """
         raise NotImplementedError
 
-    def get_fat_edges(self, vertices, edges, epsilon):
-        '''
-        For a list of edges in a graph embedded in our mesh (represented as
-        pairs of indices into `vertices`) and a width epsilon > 0, return a
-        list of lists of vertices in our mesh. Each list of vertices
-        corresponds to a fattened edge.
-        '''
+    def get_partials(self) -> np.ndarray:
+        """
+        Return the partials of each of the vertices' parameters.
 
+        We assume the mesh is parameterized by |V| scalars, each of
+        which affects exactly one vertex. For efficiency, this returns a
+        |V| by 3 array. The output of this function should be treated as
+        read only.
+        """
         raise NotImplementedError
 
-    def get_support_area(self):
-        '''
-        Return the area of the support of the mesh. For example, if the support
-        is the unit sphere, then this will return 4*pi.
-        '''
+    def get_fat_edges(
+        self,
+        vertices: typing.List[typing.List[float]],
+        edges: typing.List[typing.Tuple[int, int]],
+        epsilon: float
+    ) -> typing.List[typing.List[dcelmesh.Mesh.Vertex]]:
+        """
+        Find fattened versions of edges when mapped onto this mesh.
 
+        For a list of edges in a graph embedded in our mesh (represented
+        as pairs of indices into `vertices`) and a width `epsilon` > 0,
+        return a list of lists of vertices in our mesh. Each list of
+        vertices corresponds to a fattened edge.
+        """
+        raise NotImplementedError
+
+    def get_support_area(self) -> float:
+        """
+        Return the area of the support of the mesh.
+
+        For example, if the support is the unit sphere, then this will
+        return 4 * pi.
+        """
         raise NotImplementedError
--- src/linear_geodesic_optimization/mesh/rectangle.py
+++ src/linear_geodesic_optimization/mesh/rectangle.py
@@ -1,3 +1,7 @@
+"""Module containing an implementation of a rectangluar mesh."""
+
+import typing
+
 import dcelmesh
 import numpy as np
 
@@ -5,41 +9,48 @@ import linear_geodesic_optimization.mesh.mesh
 
 
 class Mesh(linear_geodesic_optimization.mesh.mesh.Mesh):
-    '''
-    Representation of a mesh that is "approximately" a rectangle. In
-    particular, projecting the vertices of the mesh so that their z-coordinates
-    are 0 will yield a rectangle. The mesh itself looks like a grid where each
-    cell has been cut by its major diagonal.
-    '''
+    """
+    Representation of a mesh that is "approximately" a rectangle.
 
-    def __init__(self, width, height, scale=1., extent=np.inf):
-        self._width = width
-        self._height = height
-        self._scale = scale
+    In particular, projecting the vertices of the mesh so that their
+    z-coordinates are 0 will yield a rectangle. The mesh itself looks
+    like a grid where each cell has been cut by its major diagonal.
+    """
 
-        self._topology, self._coordinates = self._get_mesh()
+    def __init__(self, width: int, height: int, scale: float = 1.,
+                 extent: float = np.inf):
+        """
+        Create a rectangular mesh.
 
-        self._extent = np.inf
+        Topologically, the mesh will have `width` vertices horizontally
+        and `height` vertices vertically. The edges are such that the
+        mesh is composed of square cells with a major diagonal.
 
-        self._partials = np.zeros((width * height, 3))
+        For the coordinate computations, the support of the mesh is
+        [-`scale` / 2, `scale` / 2]^2.
+
+        The partial derivatives are bounded between 0 and `extent`.
+        """
+        self._width: int = width
+        self._height: int = height
+        self._scale: float = scale
+
+        self._topology: dcelmesh.Mesh = self._get_topology()
+        self._coordinates: np.ndarray = self._get_coordinates()
+
+        self._extent: float = np.inf
+
+        self._parameters: np.ndarray = np.zeros(width * height)
+
+        self._updates: int = 0
+
+        self._partials: np.ndarray = np.zeros((width * height, 3))
         self._partials[:, 2] = 1.
-        self._parameters = np.zeros(width * height)
 
-        self._updates = 0
-
-    def _get_mesh(self):
+    def _get_topology(self) -> dcelmesh.Mesh:
         topology = dcelmesh.Mesh(self._width * self._height)
-        coordinates = np.zeros((self._width * self._height, 2))
 
-        # The vertices are ordered lexicographically as (x, y)
-        for i in range(self._width):
-            for j in range(self._height):
-                coordinates[i * self._height + j:] = [
-                    (i / (self._width - 1) - 0.5) * self._scale,
-                    (j / (self._height - 1) - 0.5) * self._scale
-                ]
-
-        # Add edges and faces cell-by-cell
+        # Add faces cell-by-cell
         for i in range(self._width - 1):
             for j in range(self._height - 1):
                 v00 = i * self._height + j            # Bottom-left
@@ -53,18 +64,31 @@ class Mesh(linear_geodesic_optimization.mesh.mesh.Mesh):
                 # Add the bottom-right triangle
                 topology.add_face([v00, v10, v11])
 
-        return topology, coordinates
+        return topology
 
-    def get_topology(self):
+    def _get_coordinates(self) -> np.ndarray:
+        coordinates = np.zeros((self._width * self._height, 2))
+
+        # The vertices are ordered lexicographically as (x, y)
+        for i in range(self._width):
+            for j in range(self._height):
+                coordinates[i * self._height + j:] = [
+                    (i / (self._width - 1) - 0.5) * self._scale,
+                    (j / (self._height - 1) - 0.5) * self._scale
+                ]
+
+        return coordinates
+
+    def get_topology(self) -> dcelmesh.Mesh:
+        """Return the topology of the mesh."""
         return self._topology
 
-    def get_partials(self):
-        if self._extent != np.inf:
-            self._partials = self._extent * np.exp(self._parameters) \
-                / (1 + np.exp(-self._parameters))**2
-        return self._partials
+    def get_coordinates(self) -> np.ndarray:
+        """
+        Return the coordinates of the vertices of the mesh.
 
-    def get_coordinates(self):
+        For efficiency, this returns a |V| by 3 array.
+        """
         if self._extent == np.inf:
             z = self._parameters
         else:
@@ -72,25 +96,78 @@ class Mesh(linear_geodesic_optimization.mesh.mesh.Mesh):
         return np.concatenate((self._coordinates,
                                np.reshape(z, (-1, 1))), axis=1)
 
-    def get_parameters(self):
+    def get_parameters(self) -> np.ndarray:
+        """
+        Return the z-coordinates of the vertices of this mesh.
+
+        The output is ordered in the same way as the output of
+        `Mesh.get_coordinates`.
+        """
         return self._parameters
 
-    def set_parameters(self, z):
+    def set_parameters(self, z: np.ndarray) -> np.ndarray:
+        """
+        Set the z-coordinates of the vertices of this mesh.
+
+        The input to this function should be ordered in the same way as
+        the output of `Mesh.get_coordinates`. This function returns the
+        resulting parameters, which should be treated as read only.
+        """
         if not np.array_equal(self._parameters, z):
             self._parameters = np.copy(z)
             self._updates += 1
         self._parameters
 
-    def updates(self):
+    def updates(self) -> int:
+        """
+        Return the number of calls to `Mesh.set_parameters`.
+
+        This function is an easy (O(1)) way to determine whether the
+        mesh has been updated.
+        """
         return self._updates
 
-    def get_fat_edges(self, vertices, edges, epsilon=None):
-        def is_on_fat_edge(u, v, r, epsilon):
-            '''
-            Determine whether `r` is within `epsilon` of the line segment
-            between `u` and `v`.
-            '''
+    def get_partials(self) -> np.ndarray:
+        """
+        Return the partials of each of the vertices' parameters.
 
+        If the `extent` the mesh was initialized is infinity, then each
+        partial is just a unit vertex pointing along the z-axis.
+
+        Otherwise, the partials are determined according to the logistic
+        (sigmoid) function of the corresponding parameter, scaled so
+        that the output lies between 0 and `extent`.
+        """
+        if self._extent != np.inf:
+            self._partials = self._extent * np.exp(self._parameters) \
+                / (1 + np.exp(-self._parameters))**2
+        return self._partials
+
+    def get_fat_edges(
+        self,
+        vertices: typing.List[typing.List[float]],
+        edges: typing.List[typing.Tuple[int, int]],
+        epsilon: float
+    ) -> typing.List[typing.List[dcelmesh.Mesh.Vertex]]:
+        """
+        Find fattened versions of edges when mapped onto this mesh.
+
+        For a list of edges in a graph embedded in our mesh (represented
+        as pairs of indices into `vertices`) and a width `epsilon` > 0,
+        return a list of lists of vertices in our mesh. Each list of
+        vertices corresponds to a fattened edge, where the vertices are
+        those within a distance of epsilon of the original edge when
+        everything is projected to the x-y plane.
+        """
+        def is_on_fat_edge(
+            u: np.ndarray, v: np.ndarray, r: np.ndarray, epsilon: float
+        ) -> bool:
+            """
+            Find whether a point is close to a line segment.
+
+            In detail, determine whether `r` is within `epsilon` of the
+            line segment between `u` and `v`.
+            """
             # Only care about the first two coordinates
             u = u[:2]
             v = v[:2]
@@ -107,31 +184,40 @@ class Mesh(linear_geodesic_optimization.mesh.mesh.Mesh):
             else:
                 return ru @ ru < epsilon**2 or rv @ rv < epsilon**2
 
-        return [[i for i in range(self._coordinates.shape[0])
+        return [[self._topology.get_vertex(index)
+                 for index in range(self._coordinates.shape[0])
                  if is_on_fat_edge(vertices[e1], vertices[e2],
-                                   self._coordinates[i, :], epsilon)]
+                                   self._coordinates[index, :], epsilon)]
                 for (e1, e2) in edges]
 
-    def nearest_vertex_index(self, v):
-        '''
-        Find the index of the vertex whose (x, y) coordinate pair is closest to
-        the input coordinate pair. We assume the input lies in
-        [-scale / 2, scale / 2]^2.
-        '''
+    def nearest_vertex(self, coordinate: typing.Tuple[float, float]) \
+            -> dcelmesh.Mesh.Vertex:
+        """
+        Find the closest mesh vertex to the input coordinate pair.
 
-        i = round((v[0] / self._scale + 0.5) * (self._width - 1))
-        j = round((v[1] / self._scale + 0.5) * (self._height - 1))
-        return i * self._height + j
+        Distance is measured solely by x-y coordinates. We assume the
+        input lies in the mesh's support [-`scale` / 2, `scale` / 2]^2.
+        """
+        i = round((coordinate[0] / self._scale + 0.5) * (self._width - 1))
+        j = round((coordinate[1] / self._scale + 0.5) * (self._height - 1))
+        return self._topology.get_vertex(i * self._height + j)
 
-    def map_coordinates_to_support(self, coordinates, scale_factor=1.):
-        '''
-        Convert a list of (x, y, z) triples into a list of new coordinates that
-        have been scaled to lie centered in the support. The `scale_factor`
-        parameter determines what proportion of the support is used (0.45
-        means 45% of the width and 45% of the height is used).
-        '''
+    def map_coordinates_to_support(
+            self,
+            coordinates: typing.List[typing.Tuple[float, float]],
+            scale_factor: float = 1.
+    ) -> typing.List[typing.Tuple[float, float]]:
+        """
+        Rescale coordinates so that they lie within the mesh support.
 
-        # Need this check to avoid out-of-bounds errors if coordinates is empty
+        In more detail, convert a list of (x, y) pairs into a list of
+        new coordinates that have been scaled to lie centered in the
+        support. The `scale_factor` parameter determines what proportion
+        of the support is used (0.45 means at most 45% of the width and
+        45% of the height is used).
+        """
+        # Need this check to avoid out-of-bounds errors if coordinates
+        # is empty
         if not coordinates:
             return []
 
@@ -146,13 +232,25 @@ class Mesh(linear_geodesic_optimization.mesh.mesh.Mesh):
         coordinates = coordinates - (coordinates_min + coordinates_max) / 2
         coordinates = coordinates / divisor
 
-        return list(self._scale * scale_factor * coordinates)
+        return list(
+            tuple(xy)
+            for xy in self._scale * scale_factor * coordinates
+        )
 
-    def get_support_area(self):
+    def get_support_area(self) -> float:
+        """
+        Return the support area of this mesh.
+
+        This is a simple length-times-width computation, where both of
+        them is given by the `scale` parameter passed during
+        initialization.
+        """
         return self._scale**2
 
-    def get_width(self):
+    def get_width(self) -> int:
+        """Return the number of vertices horizontally in this mesh."""
         return self._width
 
-    def get_height(self):
+    def get_height(self) -> int:
+        """Return the number of vertices vertically in this mesh."""
         return self._height
