53625d3f49fea68dd060e41d06a865f46566cbe2
Stephen Jasina <stephenjasina@gmail.com>
2023-05-16 09:56:36 -0500

    Initial implementation of unfolding computation

--- src/linear_geodesic_optimization/optimization/geodesic.py
+++ src/linear_geodesic_optimization/optimization/geodesic.py
@@ -1,6 +1,5 @@
 """Module containing utilities to compute geodesic paths."""
 
-import collections
 import itertools
 import typing
 
@@ -37,6 +36,10 @@ class Computer:
             tuple(vertex.index() for vertex in face.vertices())
             for face in self._topology.faces()
         ]
+        """
+        An explicit representation of our mesh's topology. This is
+        required as input to `meshutility`'s geodesic solver.
+        """
 
         self._u: int = u
         self._v: int = v
@@ -45,14 +48,18 @@ class Computer:
         self._forward_updates: int = self._mesh.get_updates() - 1
         self._coordinates: npt.NDArray[np.float64] \
             = np.zeros((self._topology.n_vertices(), 3))
-        self.path_edges: typing.List[typing.Tuple[int, int]] = []
+        self.path: typing.List[typing.Union[dcelmesh.Mesh.Vertex,
+                                            dcelmesh.Mesh.Halfedge]] = []
         """
-        A list of edges that the geodesic path passes through.
+        A list of vertices and edges incident to the geodesic path.
 
         For endpoints and saddle points through which path passes, the
-        pairs consist of two copies of the same point.
+        corresponding element is just a vertex. For other edges, the
+        elements are halfedges oriented such that the face the halfedge
+        points towards contains the next vertex or halfedge (i.e., its
+        the next face the path passes through).
         """
-        self.path_ratios: typing.List[float] = []
+        self.path_ratios: typing.List[np.float64] = []
         """
         Where along each edge the geodesic path passes through.
 
@@ -60,7 +67,7 @@ class Computer:
         the actual path: simply linearly interpolate between the two
         endpoints of each edge using the corresponding ratio.
         """
-        self.distance: float = 0.
+        self.distance: np.float64 = 0.
         """The geodesic distance itself."""
 
         # Reverse variables
@@ -77,12 +84,12 @@ class Computer:
         the geodesic path passes.
         """
 
-    def forward(self):
+    def forward(self) -> None:
         """
         Compute the forward direction.
 
         The computed values will be stored in the variables:
-        * `Computer.path_edges`
+        * `Computer.path`
         * `Computer.path_ratios`
         * `Computer.distance`
         """
@@ -92,15 +99,33 @@ class Computer:
         self._coordinates = self._mesh.get_coordinates()
 
         # Call the meshutility solver
-        self.path_edges, self.path_ratios = meshutility.pygeodesic.find_path(
+        path, path_ratios = meshutility.pygeodesic.find_path(
             self._coordinates, self._faces, self._u, self._v
         )
 
-        # Reconstruct the path
-        points0 = self._coordinates[self.path_edges[:,0]]
-        points1 = self._coordinates[self.path_edges[:,1]]
-        points = np.multiply(points0, 1. - self.path_ratios[:, np.newaxis]) \
-            + np.multiply(points1, self.path_ratios[:, np.newaxis])
+        # Orient path_edges sensibly. Each halfedge points to the next
+        # face the path passes through.
+        self.path = []
+        self.path_ratios = []
+        for index in range(len(path)):
+            i, j = path[index]
+            if i == j:
+                self.path.append(self._topology.get_vertex(i))
+                self.path_ratios.append(np.float64(0.))
+            elif self._topology.get_halfedge(i, j).previous().origin() \
+                    in path[index + 1]:
+                self.path.append(self._topology.get_halfedge(i, j))
+                self.path_ratios.append(path_ratios[index])
+            else:
+                self.path.append(self._topology.get_halfedge(j, i))
+                self.path_ratios.append(1 - path_ratios[index])
+
+        # Reconstruct the path. This follows the example code from
+        # https://zishun.github.io/projects/MeshUtility/
+        points_0 = self._coordinates[path[:, 0]]
+        points_1 = self._coordinates[path[:, 1]]
+        points = np.multiply(points_0, 1. - path_ratios[:, np.newaxis]) \
+            + np.multiply(points_1, path_ratios[:, np.newaxis])
 
         # The total distance is the sum of the length of each segment
         self.distance = sum([
@@ -108,43 +133,147 @@ class Computer:
             for a, b in itertools.pairwise(points)
         ])
 
-    # @staticmethod
-    # def next_():
-    #     pass
+    @staticmethod
+    def _get_next_point(
+        u: npt.NDArray,
+        v: npt.NDArray,
+        d_u: np.float64,
+        d_v: np.float64
+    ) -> npt.NDArray:
+        """
+        Find a point at a certain distance from two other points.
 
-    # def _calc_single_edge(self, a, b):
-    #     return 0.
+        Given two two-dimensional input points `u` and `v`, find a point
+        `w` so that the distance from `v` to `w` is `d_u` and the
+        distance from `u` to `w` is `d_v`. Furthermore, ensure that the
+        resulting triangle (`u`, `v`, `w`) is oriented counterclockwise.
+        """
+        d_w = np.linalg.norm(u - v)
+        h = (d_w + (d_v**2 - d_u**2) / d_w) / 2.
+        k = np.sqrt(d_v**2 - h**2)
+        rotate = np.array([[0., -1.], [1., 0.]])
+        return u + h * (v - u) + k * (rotate @ (v - u))
 
-    # def _calc_convex(self, path_edges, path_ratios):
-    #     point_locations = {}
-    #     point_locations[path_edges[0][0]] = self._vertices[path_edges[0][0]]
-    #     point_locations[path_edges[1][0]] = self._vertices[path_edges[1][0]]
-    #     point_locations[path_edges[1][1]] = self._vertices[path_edges[1][1]]
-    #     for i, (u, v) in enumerate(path_edges[2:], start=2):
-    #         if self._nxt[u,v] in path_edges[i-1]:
-    #             u, v = v, u
-    #         t = self._nxt[v,u]
-    #         w = self._nxt[u,v]
+    def _calc_point_locations(self,
+                              start: dcelmesh.Mesh.Vertex,
+                              middle: typing.List[dcelmesh.Mesh.Halfedge],
+                              end: dcelmesh.Mesh.Vertex) \
+            -> typing.Dict[int, npt.NDArray[np.float64]]:
+        """
+        Unfold a sequence of faces according to the connecting edges.
 
-    #         # Want w to be coplanar to t, u, and v
-    #         point_locations[w] = 0. # TODO
+        Return the locations of the vertices incident to the faces on
+        the geodesic path when the mesh is "unfolded." Notably, this is
+        a two-dimensional representation.
+        """
+        point_locations: typing.Dict[int, npt.NDArray[np.float64]] = {}
+        if not middle:
+            point_locations[start.index()] = np.zeros(2)
+            point_locations[end.index()] = np.array([
+                np.linalg.norm(self._coordinates[start.index()]
+                               - self._coordinates[end.index()]),
+                0.
+            ])
+            return point_locations
 
-    #     partials = collections.defaultdict(float)
-    #     mesh_partials = self._mesh.get_partials()
+        # Start by placing the first edge
+        point_locations[middle[0].origin().index()] = np.zeros(2)
+        point_locations[middle[0].destination().index()] = np.array([
+            np.linalg.norm(
+                self._coordinates[middle[0].origin().index()]
+                - self._coordinates[middle[0].destination().index()]),
+            0.
+        ])
 
-    #     return partials
+        # Place the remaining edges
+        for previous_halfedge, current_halfedge in itertools.pairwise(middle):
+            i = previous_halfedge.origin().index()
+            j = previous_halfedge.destination().index()
+            k = previous_halfedge.previous().origin().index()
 
-    def reverse(self):
+            # v_i and v_j have already had their locations computed. We
+            # need to make sure v_k is the right point to add.
+            if k != current_halfedge.origin().index() \
+                    and k != current_halfedge.destination().index():
+                i, j = j, i
+                k = previous_halfedge.twin().previous().origin().index()
+
+            point_locations[k] = Computer._get_next_point(
+                point_locations[i],
+                point_locations[j],
+                np.linalg.norm(self._coordinates[k] - self._coordinates[j]),
+                np.linalg.norm(self._coordinates[k] - self._coordinates[i])
+            )
+
+        # Place the starting point
+        i = middle[0].origin().index()
+        j = middle[0].destination().index()
+        if middle[0].previous().origin().index() != start.index():
+            i, j = j, i
+        k = start.index()
+        point_locations[k] = Computer._get_next_point(
+            point_locations[i],
+            point_locations[j],
+            np.linalg.norm(self._coordinates[k] - self._coordinates[j]),
+            np.linalg.norm(self._coordinates[k] - self._coordinates[i])
+        )
+
+        # Place the ending point
+        i = middle[-1].origin().index()
+        j = middle[-1].destination().index()
+        if middle[-1].previous().origin().index() != end.index():
+            i, j = j, i
+        k = end.index()
+        point_locations[k] = Computer._get_next_point(
+            point_locations[i],
+            point_locations[j],
+            np.linalg.norm(self._coordinates[k] - self._coordinates[j]),
+            np.linalg.norm(self._coordinates[k] - self._coordinates[i])
+        )
+
+    def _calc_convex(self,
+                     start: dcelmesh.Mesh.Vertex,
+                     middle: typing.List[dcelmesh.Mesh.Halfedge],
+                     end: dcelmesh.Mesh.Vertex) \
+            -> typing.Dict[int, np.float64]:
+        """
+        Compute the partials for a geodesic not passing through saddles.
+
+        In other words, the only mesh points the geodesic path should
+        coincide with are the endpoints.
+        """
+        partials = {}
+        mesh_partials = self._mesh.get_partials()
+
+        return partials
+
+    def reverse(self) -> None:
         self.forward()
         self._laplacian.reverse()
         if self._reverse_updates == self._mesh.get_updates():
             return
         self._reverse_updates = self._mesh.get_updates()
+        self._partials = self._mesh.get_partials()
 
-        saddle_indices = [i for i, (a, b) in enumerate(self.path_edges) if a == b]
-        print(saddle_indices)
+        # Split the path up piecewise, where boundaries are marked by
+        # vertices.
+        # At the same time, dif_distance will be computed by
+        # accumulation, so we set the relevant values to zero.
+        path_vertices: typing.List[dcelmesh.Mesh.Vertex] = []
+        path_edges: typing.List[typing.List[dcelmesh.Mesh.Halfedge]] = []
+        self.dif_distance = {}
+        for element in self.path:
+            if isinstance(element, dcelmesh.Mesh.Vertex):
+                path_vertices.append(element)
+                path_edges.append([])
+                self.dif_distance[element.index()] = np.float64(0.)
+            else:
+                path_edges[-1].append(element)
+                self.dif_distance[element.origin().index()] = np.float64(0.)
+                self.dif_distance[element.destination().index()] \
+                    = np.float64(0.)
 
-        self.partials = collections.defaultdict(float)
-        for i, j in itertools.pairwise(saddle_indices):
-            for key, value in self._calc_convex(self._path_edges[i:j+1], self._path_ratios[i:j+1]):
-                self.partials[key] += value
+        for (start, end), middle in zip(itertools.pairwise(path_vertices),
+                                        path_edges):
+            for key, value in self._calc_convex(start, middle, end):
+                self.dif_distance[key] += value
--- src/test/geodesic.py
+++ src/test/geodesic.py
@@ -1,5 +1,6 @@
 import sys
 
+import dcelmesh
 import numpy as np
 
 sys.path.append('.')
@@ -10,19 +11,11 @@ from linear_geodesic_optimization.optimization.laplacian \
     import Computer as Laplacian
 
 
-width = 30
-height = 30
+width = 15
+height = 15
 mesh = RectangleMesh(width, height)
-# z = np.array([
-#     -(16.**2
-#         - (i / (width - 1) - 0.5)**2
-#         - (j / (height - 1) - 0.5)**2)**0.5
-#     for i in range(width)
-#     for j in range(height)
-# ])
-# np.random.seed(0)
-# z = np.random.random(width * height)
-z = np.zeros(width * height)
+np.random.seed(0)
+z = np.random.random(width * height)
 z = mesh.set_parameters(z)
 
 laplacian = Laplacian(mesh)
@@ -30,4 +23,8 @@ geodesic = Geodesic(mesh, 0, width * height - 1, laplacian)
 
 geodesic.forward()
 print(geodesic.distance)
-print(geodesic.path_edges)
+for element in geodesic.path:
+    if isinstance(element, dcelmesh.Mesh.Vertex):
+        print(f'{element.index()}')
+    else:
+        print(f'({element.origin().index()}, {element.destination().index()})')
