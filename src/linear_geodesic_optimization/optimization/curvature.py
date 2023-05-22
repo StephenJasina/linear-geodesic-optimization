1be59b820fe83a506756e91079ec18d61612ea1f
Stephen Jasina <stephenjasina@gmail.com>
2023-05-22 03:49:58 -0500

    Implement reverse direction for curvature

--- src/linear_geodesic_optimization/optimization/curvature.py
+++ src/linear_geodesic_optimization/optimization/curvature.py
@@ -1,5 +1,6 @@
 """Module containing utilities to compute curvature on a mesh."""
 
+import itertools
 import typing
 
 import dcelmesh
@@ -49,13 +50,6 @@ class Computer:
         self._reverse_updates: int = mesh.get_updates() - 1
         self._partials: npt.NDArray[np.float64] \
             = np.zeros((self._topology.n_vertices(), 3))
-        self.dif_vertex_N: \
-            typing.List[typing.Dict[int, npt.NDArray[np.float64]]] \
-            = [{} for _ in self._topology.vertices()]
-        """
-        A list of partials of approximate normals at vertices, indexed
-        by vertices, and then by vertices (at most distance 1 away).
-        """
         self.dif_mean_curvature_normal: \
             typing.List[typing.Dict[int, npt.NDArray[np.float64]]] \
             = [{} for _ in self._topology.vertices()]
@@ -64,7 +58,7 @@ class Computer:
         vertices, and then by vertices (at most distance 1 away).
         """
         self.dif_kappa_G: typing.List[typing.Dict[int, np.float64]] \
-            = [np.float64(0.) for _ in self._topology.vertices()]
+            = [{} for _ in self._topology.vertices()]
         """
         A list of partials of Gaussian curvatures, indexed by vertices,
         and then by vertices (at most distance 1 away).
@@ -134,19 +128,18 @@ class Computer:
             = [np.zeros(3) for _ in self._topology.vertices()]
         for edge in self._topology.edges():
             u, v = edge.vertices()
-            laplacian_element \
-                = self._laplacian.LC_neumann_halfedges[edge.index()]
+            laplacian = self._laplacian.LC_neumann_edges[edge.index()]
             self.mean_curvature_normal[u.index()] \
-                += laplacian_element * self._coordinates[v.index()] \
-                / (-2. * self._laplacian.D[u.index()])
+                -= laplacian * self._coordinates[v.index()] \
+                / (2. * self._laplacian.D[u.index()])
             self.mean_curvature_normal[v.index()] \
-                += laplacian_element * self._coordinates[u.index()] \
-                / (-2. * self._laplacian.D[v.index()])
+                -= laplacian * self._coordinates[u.index()] \
+                / (2. * self._laplacian.D[v.index()])
         for vertex in self._topology.vertices():
             self.mean_curvature_normal[vertex.index()] \
-                += self._laplacian.LC_neumann_vertices[vertex.index()] \
+                -= self._laplacian.LC_neumann_vertices[vertex.index()] \
                 * self._coordinates[vertex.index()] \
-                / (-2. * self._laplacian.D[vertex.index()])
+                / (2. * self._laplacian.D[vertex.index()])
 
         # Compute mean and principal curvatures
         for vertex in self._topology.vertices():
@@ -170,7 +163,6 @@ class Computer:
         Compute the reverse direction (that is, partials).
 
         The computed values will be stored in the variables:
-        * `Computer.dif_vertex_N`
         * `Computer.dif_mean_curvature_normal`
         * `Computer.dif_kappa_G`
         * `Computer.dif_kappa_H`
@@ -184,139 +176,108 @@ class Computer:
         self._reverse_updates = self._mesh.get_updates()
         self._partials = self._mesh.get_partials()
 
+        # Compute partials of Gaussian curvatures
+        self.dif_kappa_G = [
+            {}
+            if vertex.is_on_boundary()
+            else {
+                near.index():
+                -self._laplacian.dif_D[vertex.index()][near.index()]
+                * self.kappa_G[vertex.index()]
+                / self._laplacian.D[vertex.index()]
+                for near in itertools.chain([vertex], vertex.vertices())
+            }
+            for vertex in self._topology.vertices()
+        ]
+        for halfedge in self._topology.halfedges():
+            u = halfedge.origin()
+            v = halfedge.destination()
+            w = halfedge.previous().origin()
+            if w.is_on_boundary():
+                continue
+            for vertex in [u, v, w]:
+                self.dif_kappa_G[w.index()][vertex.index()] \
+                    += self._laplacian.dif_cot[halfedge.index()][vertex.index()] \
+                    / (1 + self._laplacian.cot[halfedge.index()]**2) \
+                    / self._laplacian.D[w.index()]
 
-# TODO: Replace this
-class Reverse:
-    '''
-    Implementation of the gradient of the curvature loss function on a mesh.
-    This implementation assumes the l-th partial affects only the l-th vertex.
-    '''
+        # Compute partials of mean curvature normals
+        self.dif_mean_curvature_normal = [
+            {
+                near_near.index(): np.zeros(3)
+                for near in vertex.vertices()
+                for near_near in near.vertices()
+            }
+            for vertex in self._topology.vertices()
+        ]
+        for edge in self._topology.edges():
+            w, x = edge.vertices()
+            laplacian = self._laplacian.LC_neumann_edges[edge.index()]
 
-    def __init__(self, mesh, laplacian_forward=None, curvature_forward=None,
-                 laplacian_reverse=None):
-        self._mesh = mesh
-        self._updates = self._mesh.updates() - 1
-        self._v = None
-        self._e = self._mesh.get_edges()
-        self._nxt = self._mesh.get_nxt()
+            for u, v in [(w, x), (x, w)]:
+                vertex_area = self._laplacian.D[u.index()]
+                self.dif_mean_curvature_normal[u.index()][v.index()] \
+                    -= self._laplacian.LC_neumann_edges[edge.index()] \
+                    * self._partials[v.index()] \
+                    / (2. * vertex_area)
+                for near in itertools.chain([u], u.vertices()):
+                    dif_vertex_area \
+                        = self._laplacian.dif_D[u.index()][near.index()]
+                    if near.index() \
+                            in self._laplacian.dif_LC_neumann_edges[edge.index()]:
+                        dif_laplacian \
+                            = self._laplacian.dif_LC_neumann_edges[edge.index()][near.index()]
+                    else:
+                        dif_laplacian = np.float64(0.)
+                    self.dif_mean_curvature_normal[u.index()][near.index()] \
+                        -= (dif_laplacian
+                            - dif_vertex_area * laplacian / vertex_area) \
+                        * self._coordinates[v.index()] \
+                        / (2. * vertex_area)
+        for vertex in self._topology.vertices():
+            coordinates = self._coordinates[vertex.index()]
+            vertex_area = self._laplacian.D[vertex.index()]
+            laplacian = self._laplacian.LC_neumann_vertices[vertex.index()]
+            self.dif_mean_curvature_normal[vertex.index()][vertex.index()] \
+                -= self._laplacian.LC_neumann_vertices[vertex.index()] \
+                * self._partials[vertex.index()] \
+                / (2. * vertex_area)
+            for near in itertools.chain([vertex], vertex.vertices()):
+                dif_vertex_area = self._laplacian.dif_D[vertex.index()][near.index()]
+                dif_laplacian \
+                    = self._laplacian.dif_LC_neumann_vertices[vertex.index()][near.index()]
+                self.dif_mean_curvature_normal[vertex.index()][near.index()] \
+                    -= ((dif_laplacian
+                         - dif_vertex_area * laplacian / vertex_area)
+                        * coordinates) \
+                    / (2. * vertex_area)
 
-        self._V = len(self._e)
-
-        self._dif_v = None
-        self._l = None
-
-        self._laplacian_forward = laplacian_forward
-        if self._laplacian_forward is None:
-            self._laplacian_forward = laplacian.Forward(mesh)
-
-        self._curvature_forward = curvature_forward
-        if self._curvature_forward is None:
-            self._curvature_forward = Forward(mesh, self._laplacian_forward)
-
-        self._laplacian_reverse = laplacian_reverse
-        if self._laplacian_reverse is None:
-            self._laplacian_reverse = laplacian.Reverse(mesh)
-
-        self._N = None
-        self._D_inv = None
-        self._cot = None
-        self._LC = None
-
-        self._kappa_G = None
-        self._vertex_N = None
-        self._mean_curvature_normal = None
-
-        self._dif_N = None
-        self._dif_D = None
-        self._dif_cot = None
-        self._dif_LC = None
-
-        # Derivatives match the types of what are being differentiated.
-        self.dif_kappa_G = None
-        self.dif_vertex_N = None
-        self.dif_mean_curvature_normal = None
-        self.dif_kappa_H = None
-        self.dif_kappa_1 = None
-        self.dif_kappa_2 = None
-
-    def _calc_dif_kappa_G(self):
-        dif_Dkappa_G = np.zeros(self._V)
-        for (i, j), cot_ij in self._cot.items():
-            if (i, j) in self._dif_cot:
-                dif_Dkappa_G[self._nxt[i,j]] += self._dif_cot[i,j] / (1 + cot_ij**2)
-        return self._D_inv @ (dif_Dkappa_G - self._dif_D @ self._kappa_G)
-
-    def _calc_dif_vertex_N(self):
-        dif_vertex_normal = np.zeros((self._V, 3))
-        for i in range(self._V):
-            for j in self._e[i]:
-                dif_N = self._dif_N[i, j] if (i, j) in self._dif_N else 0.
-                dif_vertex_normal[i,:] += dif_N
-        return dif_vertex_normal
-
-    def _calc_dif_mean_curvature_normal(self):
-        dif_v = np.zeros((self._V, 3))
-        dif_v[self._l,:] = self._dif_v
-        return -self._D_inv @ (
-            (self._dif_LC - self._dif_D @ self._D_inv @ self._LC) @ self._v
-            + self._LC @ dif_v
-        ) / 2
-
-    def _calc_dif_kappa_H(self):
-        dif_kappa_H = np.zeros(self._V)
-        for i in range(self._V):
-            vn = self._vertex_N[i,:]
-            mcn = self._mean_curvature_normal[i,:]
-            dif_mcn = self.dif_mean_curvature_normal[i,:]
-            dif_kappa_H[i] = np.sign(vn @ mcn) * (mcn @ dif_mcn) / np.linalg.norm(mcn)
-        return dif_kappa_H
-
-    def _calc_dif_kappa_1(self):
-        return np.divide(
-            2 * self._kappa_1 * self.dif_kappa_H - self.dif_kappa_G,
-            self._kappa_1 - self._kappa_2,
-            np.copy(self.dif_kappa_H),
-            where=(self._kappa_1 != self._kappa_2)
-        )
-
-    def _calc_dif_kappa_2(self):
-        return np.divide(
-            self.dif_kappa_G - 2 * self._kappa_2 * self.dif_kappa_H,
-            self._kappa_1 - self._kappa_2,
-            np.copy(self.dif_kappa_H),
-            where=(self._kappa_1 != self._kappa_2)
-        )
-
-    def calc(self, dif_v, l):
-        self._laplacian_forward.calc()
-        self._N = self._laplacian_forward.N
-        self._D_inv = self._laplacian_forward.D_inv
-        self._cot = self._laplacian_forward.cot
-        self._LC = self._laplacian_forward.LC_neumann
-
-        self._curvature_forward.calc()
-        self._kappa_G = self._curvature_forward.kappa_G
-        self._vertex_N = self._curvature_forward.vertex_N
-        self._mean_curvature_normal = self._curvature_forward.mean_curvature_normal
-        self._kappa_H = self._curvature_forward.kappa_H
-        self._kappa_1 = self._curvature_forward.kappa_1
-        self._kappa_2 = self._curvature_forward.kappa_2
-
-        self._laplacian_reverse.calc(dif_v, l)
-        self._dif_N = self._laplacian_reverse.dif_N
-        self._dif_D = self._laplacian_reverse.dif_D
-        self._dif_cot = self._laplacian_reverse.dif_cot
-        self._dif_LC = self._laplacian_reverse.dif_LC_neumann
-
-        if self._updates != self._mesh.updates() or self._l != l:
-            self._updates = self._mesh.updates()
-            self._v = self._mesh.get_vertices()
-            self._dif_v = dif_v
-            self._l = l
-
-            self.dif_kappa_G = self._calc_dif_kappa_G()
-            self.dif_vertex_N = self._calc_dif_vertex_N()
-            self.dif_mean_curvature_normal = self._calc_dif_mean_curvature_normal()
-            self.dif_kappa_H = self._calc_dif_kappa_H()
-            self.dif_kappa_1 = self._calc_dif_kappa_1()
-            self.dif_kappa_2 = self._calc_dif_kappa_2()
+        # Compute partials of mean and principal curvatures
+        for vertex in self._topology.vertices():
+            if vertex.is_on_boundary():
+                continue
+            mean_curvature_normal = self.mean_curvature_normal[vertex.index()]
+            vertex_normal = self.vertex_N[vertex.index()]
+            kappa_1 = self.kappa_1[vertex.index()]
+            kappa_2 = self.kappa_2[vertex.index()]
+            curvature_difference = kappa_1 - kappa_2
+            for near in itertools.chain([vertex], vertex.vertices()):
+                dif_kappa_G = self.dif_kappa_G[vertex.index()][near.index()]
+                dif_mean_curvature_normal \
+                    = self.dif_mean_curvature_normal[vertex.index()][near.index()]
+                dif_kappa_H = np.sign(vertex_normal @ mean_curvature_normal) \
+                    * (mean_curvature_normal @ dif_mean_curvature_normal) \
+                    / np.linalg.norm(mean_curvature_normal)
+                self.dif_kappa_H[vertex.index()][near.index()] = dif_kappa_H
+                if curvature_difference == np.float64(0.):
+                    self.dif_kappa_1[vertex.index()][near.index()] \
+                        = np.float64(0.)
+                    self.dif_kappa_2[vertex.index()][near.index()] \
+                        = np.float64(0.)
+                else:
+                    self.dif_kappa_1[vertex.index()][near.index()] \
+                        = (2. * kappa_1 * dif_kappa_H - dif_kappa_G) \
+                        / curvature_difference
+                    self.dif_kappa_2[vertex.index()][near.index()] \
+                        = (dif_kappa_G - 2. * kappa_2 * dif_kappa_H) \
+                        / curvature_difference
