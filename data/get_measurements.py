1b9700d96a5e2f4225d92ad36f69906102c42329
Stephen Jasina <stephenjasina@gmail.com>
2023-08-31 02:20:49 -0500

    Add guard for pings marked as -1

--- data/get_measurements.py
+++ data/get_measurements.py
@@ -4,7 +4,7 @@ import datetime
 from ripe.atlas.cousteau import AtlasResultsRequest
 
 date_start = datetime.datetime(year=2023, month=8, day=29)
-date_end = date_start + datetime.timedelta(hours=1)
+date_end = date_start + datetime.timedelta(days=1)
 
 probes = {}
 with open('probes.csv', 'r') as probes_file:
@@ -26,15 +26,24 @@ for probe_destination in probes:
             probe_ids = [id_source]
         ).create()
 
-        if not is_success or not results:
+        if not is_success:
+            print(f'Invalid query for measurement {measurement_id} '
+                  + f'between probes {id_source} and {id_destination}')
+            continue
+
+        latencies = [
+            result['min']
+            for result in results
+            if result['min'] > 0
+        ]
+        if not latencies:
             print(f'No results for measurement {measurement_id} '
                   + f'between probes {id_source} and {id_destination}')
             continue
 
-        rtts[id_source,id_destination] = min([
-            result['min']
-            for result in results
-        ])
+        rtt = min(latencies)
+        rtts[id_source,id_destination] = rtt
+        print(f'{id_source},{id_destination},{rtt}')
 
 with open('latencies.csv', 'w') as latencies_file:
     writer = csv.DictWriter(
