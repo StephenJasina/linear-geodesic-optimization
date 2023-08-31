fec7cd40eac06166e5cc60560c59f8741cabff34
Stephen Jasina <stephenjasina@gmail.com>
2023-08-31 14:46:56 -0500

    Add multithreading and better error detection

--- data/get_measurements.py
+++ data/get_measurements.py
@@ -1,59 +1,71 @@
+import concurrent.futures
 import csv
 import datetime
 
 from ripe.atlas.cousteau import AtlasResultsRequest
 
 date_start = datetime.datetime(year=2023, month=8, day=29)
-date_end = date_start + datetime.timedelta(days=1)
+date_stop = date_start + datetime.timedelta(days=1)
+rtts = {}
+def get_rtt(id_measurement, id_source, id_target):
+    while True:
+        is_success, results = AtlasResultsRequest(
+            msm_id = id_measurement,
+            start = date_start,
+            stop = date_stop,
+            probe_ids = [id_source]
+        ).create()
+
+        if is_success:
+            latencies = [
+                result['min']
+                for result in results
+                if result['min'] > 0
+            ]
+            if not latencies:
+                print(f'No results for measurement {id_measurement} '
+                      + f'between probes {id_source} and {id_target}')
+                return None
+
+            rtt = min(latencies)
+            rtts[id_source,id_target] = rtt
+            print(f'{id_source},{id_target},{rtt}')
+            return rtt
+
+        if 'MaxRetryError' not in str(results):
+            print(f'Invalid query for measurement {id_measurement} '
+                  + f'between probes {id_source} and {id_target}')
+            return None
 
 probes = {}
 with open('probes.csv', 'r') as probes_file:
     reader = csv.DictReader(probes_file)
     probes = [row for row in reader]
 
-rtts = {}
-for probe_destination in probes:
-    measurement_id = probe_destination['measurement_id']
-    id_destination = probe_destination['id']
-    for probe_source in probes:
-        id_source = probe_source['id']
-        if id_source == id_destination:
-            continue
-        is_success, results = AtlasResultsRequest(
-            msm_id = measurement_id,
-            start = date_start,
-            end = date_end,
-            probe_ids = [id_source]
-        ).create()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = []
+    for probe_target in probes:
+        id_measurement = probe_target['measurement_id']
+        id_target = probe_target['id']
+        for probe_source in probes:
+            id_source = probe_source['id']
+            futures.append(
+                executor.submit(get_rtt,
+                                id_measurement, id_source, id_target)
+            )
 
-        if not is_success:
-            print(f'Invalid query for measurement {measurement_id} '
-                  + f'between probes {id_source} and {id_destination}')
-            continue
-
-        latencies = [
-            result['min']
-            for result in results
-            if result['min'] > 0
-        ]
-        if not latencies:
-            print(f'No results for measurement {measurement_id} '
-                  + f'between probes {id_source} and {id_destination}')
-            continue
-
-        rtt = min(latencies)
-        rtts[id_source,id_destination] = rtt
-        print(f'{id_source},{id_destination},{rtt}')
+    for future in futures:
+        future.result()
 
 with open('latencies.csv', 'w') as latencies_file:
     writer = csv.DictWriter(
         latencies_file,
-        ['source_id', 'destination_id', 'rtt']
+        ['source_id', 'target_id', 'rtt']
     )
     writer.writeheader()
-    for (id_source, id_destination), rtt in sorted(rtts.items()):
+    for (id_source, id_target), rtt in sorted(rtts.items()):
         writer.writerow({
             'source_id': id_source,
-            'destination_id': id_destination,
+            'target_id': id_target,
             'rtt': rtt
         })
