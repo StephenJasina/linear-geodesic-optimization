20b19c2564e82c2ba808ce93b4106b94ec04cf55
Stephen Jasina <stephenjasina@gmail.com>
2023-08-31 15:07:52 -0500

    Move graph creation to its own function
    Also, improve style and commenting

--- data/csv_to_graphml.py
+++ data/csv_to_graphml.py
@@ -12,6 +12,8 @@ import numpy as np
 def get_GCD_latency(latlong_a, latlong_b):
     radius_earth = 40075016.68557849 / (2 * np.pi)
     c = 299792458
+
+    # Convert spherical coordinates to Cartesian coordinates
     latlong_a = np.array(latlong_a) * np.pi / 180
     p_a = np.array([
         np.cos(latlong_a[1]) * np.cos(latlong_a[0]),
@@ -24,69 +26,81 @@ def get_GCD_latency(latlong_a, latlong_b):
         np.sin(latlong_b[1]) * np.cos(latlong_b[0]),
         np.sin(latlong_b[0])
     ])
+
+    # Note that the dot product is clamped to [-1, 1] to deal with
+    # floating point error
     return np.arccos(min(max(p_a @ p_b, -1.), 1.)) \
         * radius_earth / (2. * c / 3.)
 
-if __name__ == '__main__':
-    # Parse arugments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--latencies-file', '-l', metavar='<filename>',
-                        dest='latenciesFilename', required=True)
-    parser.add_argument('--coordinates-file', '-c', metavar='<filename>',
-                        dest='coordinatesFilename', required=True)
-    parser.add_argument('--epsilon', '-e', metavar='<epsilon>',
-                        dest='epsilon', type=float, required=False)
-    parser.add_argument('--output-file', '-o', metavar='<filename>',
-                        dest='outputFilename', required=False)
-    args = parser.parse_args()
-    latenciesFilename = args.latenciesFilename
-    coordinatesFilename = args.coordinatesFilename
-    epsilon = args.epsilon
-    if epsilon is None:
-        epsilon = np.inf
-    outputFilename = args.outputFilename
-
+def get_graph(probes_file, latencies_file, epsilon):
     # Create the graph
     graph = nx.Graph()
 
-    with open(coordinatesFilename, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            graph.add_node(
-                row['id'],
-                city=row['city'], country=row['country'],
-                lat=float(row['latitude']), long=float(row['longitude'])
-            )
+    # Get the vertecies
+    probes_reader = csv.DictReader(probes_file)
+    for row in probes_reader:
+        graph.add_node(
+            row['id'],
+            city=row['city'], country=row['country'],
+            lat=float(row['latitude']), long=float(row['longitude'])
+        )
 
-    with open(latenciesFilename, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            id_source = row['source_id']
-            id_target = row['target_id']
-            lat_source = graph.nodes[id_source]['lat']
-            long_source = graph.nodes[id_source]['long']
-            lat_target = graph.nodes[id_target]['lat']
-            long_target = graph.nodes[id_target]['long']
-            latency = float(row['rtt'])
-            if (
-                latency - get_GCD_latency(
-                    [lat_source, long_source],
-                    [lat_target, long_target]
-                )
-            ) < epsilon:
-                graph.add_edge(id_source, id_target, weight=1.)
+    # Get the edges
+    latencies_reader = csv.DictReader(latencies_file)
+    for row in latencies_reader:
+        id_source = row['source_id']
+        id_target = row['target_id']
+        lat_source = graph.nodes[id_source]['lat']
+        long_source = graph.nodes[id_source]['long']
+        lat_target = graph.nodes[id_target]['lat']
+        long_target = graph.nodes[id_target]['long']
+        latency = float(row['rtt'])
+
+        # Only add edges satisfying the cutoff requirement
+        if (
+            latency - get_GCD_latency(
+                [lat_source, long_source],
+                [lat_target, long_target]
+            )
+        ) < epsilon:
+            graph.add_edge(id_source, id_target, weight=1.)
 
     # Compute the curvatures
     orc = OllivierRicci(graph, weight='weight', alpha=0.)
     graph = orc.compute_ricci_curvature()
 
     # Delete extraneous edge weights
-    for u, v, d in graph.edges(data=True):
+    for _, _, d in graph.edges(data=True):
         del[d['weight']]
 
-    # Write the graphml
-    if outputFilename is None:
-        for line in nx.generate_graphml(graph):
-            print(line)
-    else:
-        nx.write_graphml(graph, outputFilename)
+    return graph
+
+if __name__ == '__main__':
+    # Parse arugments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--latencies-file', '-l', metavar='<filename>',
+                        dest='latencies_filename', required=True)
+    parser.add_argument('--probes-file', '-p', metavar='<filename>',
+                        dest='probes_filename', required=True)
+    parser.add_argument('--epsilon', '-e', metavar='<epsilon>',
+                        dest='epsilon', type=float, required=False)
+    parser.add_argument('--output-file', '-o', metavar='<filename>',
+                        dest='output_filename', required=False)
+    args = parser.parse_args()
+    latencies_filename = args.latencies_filename
+    probes_filename = args.probes_filename
+    epsilon = args.epsilon
+    if epsilon is None:
+        epsilon = np.inf
+    output_filename = args.output_filename
+
+    with open(probes_filename, 'r') as probes_file, \
+            open(latencies_filename, 'r') as latencies_file:
+        graph = get_graph(probes_file, latencies_file, epsilon)
+
+        # Write the graphml
+        if output_filename is None:
+            for line in nx.generate_graphml(graph):
+                print(line)
+        else:
+            nx.write_graphml(graph, output_filename)
