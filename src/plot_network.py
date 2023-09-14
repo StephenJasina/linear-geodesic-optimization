f50a3a60db07d213fd3a8da6dc3280c8394de8b2
Stephen Jasina <stephenjasina@gmail.com>
2023-09-14 11:31:49 -0500

    Simplify network plotting

--- src/plot_network.py
+++ src/plot_network.py
@@ -1,42 +1,14 @@
-import json
 import os
 
 from matplotlib import pyplot as plt
-import numpy as np
+import networkx as nx
 
-from linear_geodesic_optimization import data
-from linear_geodesic_optimization.mesh.rectangle import Mesh as RectangleMesh
-from linear_geodesic_optimization.plot import get_heat_map
+from linear_geodesic_optimization.plot import get_network_plot
 
-width = 50
-height = 50
-mesh = RectangleMesh(width, height)
-
-# cutoff = 16
-# network_name = f'Graph U.S. ({cutoff})'
-# data_file_name = os.path.join('graph_US', f'graph{cutoff}.graphml')
-network_name = 'Toy'
-data_file_name = os.path.join('toy', 'toy.graphml')
+cutoff = 7
+network_name = f'Graph Europe ({cutoff})'
+data_file_name = os.path.join('graph_Europe_clustered', f'graph{cutoff}.graphml')
 data_file_path = os.path.join('..', 'data', data_file_name)
-data_name, data_type = os.path.splitext(os.path.basename(data_file_name))
-
-coordinates, network_edges, network_curvatures, network_latencies = data.read_graphml(data_file_path)
-coordinates = np.array(coordinates)
-network_vertices = mesh.map_coordinates_to_support(coordinates, 0.8)
-
-mesh.set_parameters(np.random.random(mesh.get_parameters().shape))
-vertices = mesh.get_coordinates()
-x = list(sorted(set(vertices[:,0])))
-y = list(sorted(set(vertices[:,1])))
-z = vertices[:,2].reshape(len(x), len(y))
-
-width = len(x)
-height = len(y)
-
-z = z - np.amin(z)
-
-# network_curvatures = [None] * len(network_curvatures)
-heat_map = get_heat_map(x, y, z * 0., network_name,
-                        network_vertices, network_edges, network_curvatures, network_vertices)
-heat_map.savefig('network.png', dpi=1000)
+network_plot = get_network_plot(nx.graphml.read_graphml(data_file_path))
+network_plot.savefig('network.png', dpi=1000, bbox_inches='tight')
 plt.show()
