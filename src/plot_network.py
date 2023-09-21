68b7f0c74e32cf2f0c4cbb68df78cdec83e398e5
Stephen Jasina <stephenjasina@gmail.com>
2023-09-21 11:07:00 -0500

    Remove extraneous variable

--- src/plot_network.py
+++ src/plot_network.py
@@ -6,7 +6,6 @@ import networkx as nx
 from linear_geodesic_optimization.plot import get_network_plot
 
 cutoff = 7
-network_name = f'Graph Europe ({cutoff})'
 data_file_name = os.path.join('graph_Europe_clustered', f'graph{cutoff}.graphml')
 data_file_path = os.path.join('..', 'data', data_file_name)
 network_plot = get_network_plot(nx.graphml.read_graphml(data_file_path))
