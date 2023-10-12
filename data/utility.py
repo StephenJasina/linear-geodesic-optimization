Fix GCD latency computation

data/utility.py
@@ -23,10 +23,10 @@ def get_spherical_distance(a, b):
     return np.arccos(min(max(a @ b, -1.), 1.))
 
 def get_GCD_latency(latlong_a, latlong_b):
-    """Find the great circle latency between two points on Earth."""
+    """Find the great circle latency between two points on Earth in ms."""
     # The following are in meters and meters per second
     circumference_earth = 40075016.68557849
-    radius_earth = 40075016.68557849 / (2 * np.pi)
+    radius_earth = circumference_earth / (2 * np.pi)
     c = 299792458
 
     # Convert spherical coordinates to Cartesian coordinates
@@ -35,4 +35,5 @@ def get_GCD_latency(latlong_a, latlong_b):
 
     # Compute the latency, which is the travel time at the rate of two
     # thirds the speed of light
-    return get_spherical_distance(p_a, p_b) * radius_earth / (2. * c / 3.)
+    return 2 * 1000 * get_spherical_distance(p_a, p_b) * radius_earth \
+        / (2. * c / 3.)