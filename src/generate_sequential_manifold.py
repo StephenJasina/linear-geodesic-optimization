ad50ff1d767ce943319a91dc07672c76f863814d
Stephen Jasina <stephenjasina@gmail.com>
2023-10-27 16:41:43 -0500

    Make sequential manifold generation more general

--- src/generate_sequential_manifold.py
+++ src/generate_sequential_manifold.py
@@ -1,26 +1,34 @@
+import itertools
 import os
+
+import numpy as np
 
 import optimization
 
-project_dir = '/Users/loqmansalamatian/Documents/GitHub/linear-geodesic-optimization/'
 if __name__ == '__main__':
     lambda_curvature = 1.
-    lambda_smooth = 0.004
+    lambda_smooth = 0.0004
     lambda_geodesic = 0.
     initial_radius = 20.
-    threshold = 8
     sides = 50
     width = height = sides
     scale = 1.
-    ip_type = 'ipv4'
     leaveout_proportion = 1.
+    ip_type = 'ipv4'
 
-    max_iterations = 200
-    for i in range(24):
+    input_dir = os.path.join(ip_type, 'graph_Europe_clustered_fine_threshold')
+    output_dir = os.path.join('..', f'out_{os.path.basename(input_dir)[6:]}')
+
+    max_iterations = 2
+    i_values = [
+        '{:.2f}'.format(i)
+        for i in np.arange(1., 15.25, 0.25)
+    ]
+    for i, i_previous in zip(i_values, itertools.chain([None], i_values)):
         initialiazation_file_path = None
-        if i != 0:
+        if i_previous is not None:
             directory = os.path.join(
-                project_dir, ip_type, 'out_Europe_hourly', f'graph_{i - 1}_{threshold}',
+                output_dir, f'graph_{i_previous}',
                 f'{lambda_curvature}_{lambda_smooth}_{lambda_geodesic}_{initial_radius}_{width}_{height}_{scale}'
             )
             iteration = max(
@@ -31,11 +39,10 @@ if __name__ == '__main__':
             initialiazation_file_path = os.path.join(directory, str(iteration))
 
         optimization.main(
-            os.path.join(ip_type, 'graph_Europe_hourly', str(threshold), f'graph_{i}_{threshold}.graphml'),
-            os.path.join(ip_type, 'graph_Europe_hourly', f'latencies_{i}.csv'),
+            os.path.join(input_dir, f'graph_{i}.graphml'),
+            os.path.join(input_dir, '..', 'graph_Europe', 'latencies.csv'), # maybe change this in the future
             lambda_curvature, lambda_smooth, lambda_geodesic,
             initial_radius, sides, scale, leaveout_proportion,
             max_iterations,
-            os.path.join(ip_type, 'out_Europe_hourly'),
-            initialiazation_file_path
+            output_dir, initialiazation_file_path
         )
