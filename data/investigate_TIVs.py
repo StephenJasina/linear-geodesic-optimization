33c71f6901427128c0d4ece8a9b391ae71c8c6e4
Stephen Jasina <stephenjasina@gmail.com>
2023-12-13 05:17:54 -0600

    Improve estimation of violating edges and vertices
    This uses a greedy approximation to a set cover to estimate the minimal
    number of edges (or vertices) that would need to be removed to be left
    with a TIV-free graph.

--- data/investigate_TIVs.py
+++ data/investigate_TIVs.py
@@ -1,12 +1,107 @@
 import argparse
 import csv
+import collections
 import os
 import sys
+import typing
 
 import matplotlib.pyplot as plt
+import networkx as nx
 
 import csv_to_graphml
 
+
+def compute_triangles(graph: nx.Graph, weight: str = 'rtt') \
+        -> typing.Dict[typing.Tuple[str, str, str],
+                       typing.Tuple[float, float, float]]:
+    triangles: typing.Dict[typing.Tuple[str, str, str],
+                           typing.Tuple[float, float, float]] = {}
+    for u, v, data in graph.edges(data=True):
+        weight_uv = data[weight]
+        u, v = min(u, v), max(u, v)
+        for w in graph.nodes:
+            if w <= v:
+                continue
+
+            if (u, w) in graph.edges and (v, w) in graph.edges:
+                weight_uw = graph.edges[u,w][weight]
+                weight_vw = graph.edges[v,w][weight]
+
+                triangles[u,v,w] = (weight_vw, weight_uw, weight_uv)
+
+    return triangles
+
+def compute_goodnesses(
+    triangles: typing.Dict[typing.Tuple[str, str, str],
+                           typing.Tuple[float, float, float]],
+    strategy: str = 'r'
+) -> typing.Dict[typing.Tuple[str, str, str], float]:
+    goodnesses: typing.Dict[typing.Tuple[str, str, str], float] = {}
+    for triangle, (weight_uv, weight_uw, weight_vw) in triangles.items():
+        weight_max = max(weight_uv, weight_uw, weight_vw)
+        weight_sum = weight_uv + weight_uw + weight_vw
+        goodness = weight_max / (weight_sum - weight_max)
+        if strategy == 'r':
+            goodness *= (1 + 2 * weight_max - weight_sum)
+        goodnesses[triangle] = goodness
+
+    return goodnesses
+
+def plot_goodnesses(goodnesses):
+    fig = plt.figure()
+    ax_1, ax_2 = fig.subplots(1, 2, sharey=True)
+    if goodnesses:
+        ax_1.ecdf(goodnesses.values())
+    ax_1.set_title('Goodness (All Triangles)')
+    ax_1.set_xlabel('Goodness (lower = better)')
+    ax_1.set_ylabel('CDF')
+
+    goodness_violators = [goodness for goodness in goodnesses.values() if goodness > 1]
+    if goodness_violators:
+        ax_2.ecdf(goodness_violators)
+    ax_2.set_title('Goodness (Violators Only)')
+    ax_2.set_xlabel('Goodness (lower = better)')
+    ax_2.set_ylabel('CDF')
+    plt.show()
+
+def compute_greedy_set_cover(sets: typing.Dict[typing.Hashable,
+                                               typing.Set[typing.Any]]) \
+        -> typing.List[typing.Hashable]:
+    current_cover = set()
+    candidate_sets = dict(sets)
+    set_cover: typing.List[typing.Hashable] = []
+    while True:
+        # Find the candidate with the most uncovered elements
+        best_candidate = None
+        best_candidate_set = None
+        best_candidate_size = 0
+        removable_candidates = []
+        for candidate, candidate_set in candidate_sets.items():
+            # Small optimization to avoid unnecessary difference computations
+            if len(candidate_set) <= best_candidate_size:
+                continue
+
+            candidate_size = len(candidate_set.difference(current_cover))
+
+            # Remove sets that are already covered
+            if candidate_size == 0:
+                removable_candidates.append(candidate)
+                continue
+
+            if candidate_size > best_candidate_size:
+                best_candidate = candidate
+                best_candidate_set = candidate_set
+                best_candidate_size = candidate_size
+
+        # We're done if no elements are uncovered
+        if best_candidate is None:
+            return set_cover
+
+        for candidate in removable_candidates:
+            del candidate_sets[candidate]
+
+        set_cover.append(best_candidate)
+        current_cover = current_cover.union(best_candidate_set)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -30,55 +125,34 @@ if __name__ == '__main__':
         probes_filename, latencies_filename, epsilon, 500000, 2
     )
 
-    goodnesses = []
-    triangle_count = 0
-    tiv_count = 0
-    tiv_edges = set()
-    for u, v, data in graph.edges(data=True):
-        rtt_uv = data['rtt']
-        for w in graph.nodes:
-            if (u, w) in graph.edges and (v, w) in graph.edges:
-                triangle_count += 1
-                rtt_uw = graph.edges[u,w]['rtt']
-                rtt_vw = graph.edges[v,w]['rtt']
+    triangles = compute_triangles(graph)
+    goodnesses = compute_goodnesses(triangles)
 
-                rtt_max = max(rtt_uv, rtt_uw, rtt_vw)
-                rtt_sum = rtt_uv + rtt_uw + rtt_vw
-                goodness = rtt_max / (rtt_sum - rtt_max) * (1 + 2 * rtt_max - rtt_sum)
-                goodnesses.append(goodness)
+    tivs = [
+        triangle
+        for triangle, goodness in goodnesses.items()
+        if goodness > 1.
+    ]
 
-                if 2 * rtt_max > rtt_sum:
-                    tiv_count += 1
+    tiv_edge_sets = collections.defaultdict(set)
+    for i, (u, v, w) in enumerate(tivs):
+        tiv_edge_sets[u,v].add(i)
+        tiv_edge_sets[u,w].add(i)
+        tiv_edge_sets[v,w].add(i)
+    tiv_edge_set_cover = compute_greedy_set_cover(tiv_edge_sets)
 
-                    if rtt_uv > rtt_uw:
-                        if rtt_uv > rtt_vw:
-                            tiv_edges.add((min(u, v), max(u, v)))
-                        else:
-                            tiv_edges.add((min(v, w), max(v, w)))
-                    else:
-                        if rtt_uw > rtt_vw:
-                            tiv_edges.add((min(u, w), max(u, w)))
-                        else:
-                            tiv_edges.add((min(v, w), max(v, w)))
+    tiv_vertex_sets = collections.defaultdict(set)
+    for i, (u, v, w) in enumerate(tivs):
+        tiv_vertex_sets[u].add(i)
+        tiv_vertex_sets[v].add(i)
+        tiv_vertex_sets[w].add(i)
+    tiv_vertex_set_cover = compute_greedy_set_cover(tiv_vertex_sets)
 
-                    print(f'TIV with goodness {goodness:.4f}')
+    print(f'Proportion of TIVs (triangles): {(len(tivs) / len(goodnesses)):.4f}')
+    print('Approximate proportion of TIVs (edges): '
+          + f'{(len(tiv_edge_set_cover) / len(graph.edges)):.4f}')
+    print('Approximate proportion of TIVs (vertices): '
+          + f'{(len(tiv_vertex_set_cover) / len(graph.nodes)):.4f}')
+    print(f'Worst goodness: {max(goodnesses.values()):.4f}')
 
-    print(f'Proportion of TIVs (triangles): {(tiv_count / triangle_count):.4f}')
-    print(f'Proportion of TIVs (edges): {(len(tiv_edges) / len(graph.edges)):.4f}')
-    print(f'Worst goodness: {max(goodnesses):.4f}')
-
-    fig = plt.figure()
-    ax_1, ax_2 = fig.subplots(1, 2, sharey=True)
-    if goodnesses:
-        ax_1.ecdf(goodnesses)
-    ax_1.set_title('Goodness (All Triangles)')
-    ax_1.set_xlabel('r')
-    ax_1.set_ylabel('CDF')
-
-    goodness_violators = [goodness for goodness in goodnesses if goodness > 1]
-    if goodness_violators:
-        ax_2.ecdf([goodness for goodness in goodnesses if goodness > 1])
-    ax_2.set_title('Goodness (Violators Only)')
-    ax_2.set_xlabel('r')
-    ax_2.set_ylabel('CDF')
-    plt.show()
+    plot_goodnesses(goodnesses)
