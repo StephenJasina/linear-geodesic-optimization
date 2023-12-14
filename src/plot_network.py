dcf548acea3ca6e9cceca5e245698ea400b68a48
Stephen Jasina <stephenjasina@gmail.com>
2023-12-13 23:53:01 -0600

    Add CLI call

--- src/plot_network.py
+++ src/plot_network.py
@@ -1,13 +1,21 @@
-import os
+import argparse
 
 from matplotlib import pyplot as plt
 import networkx as nx
 
 from linear_geodesic_optimization.plot import get_network_plot
 
-cutoff = 20
-data_file_name = os.path.join('ipv4', 'graph_Europe', f'graph{cutoff}.graphml')
-data_file_path = os.path.join('..', 'data', data_file_name)
-network_plot = get_network_plot(nx.graphml.read_graphml(data_file_path))
-# network_plot.savefig('network.png', dpi=1000, bbox_inches='tight')
-plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_filename', metavar='graphml-file')
+    parser.add_argument('--output', '-o', metavar='filename',
+                        dest='output_filename')
+    args = parser.parse_args()
+    input_filename = args.input_filename
+    output_filename = args.output_filename
+
+    network_plot = get_network_plot(nx.graphml.read_graphml(input_filename))
+    if output_filename is not None:
+        network_plot.savefig('network.png', dpi=1000, bbox_inches='tight')
+    plt.show()
