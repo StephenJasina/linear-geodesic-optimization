3b8e05dd5505360156a9189eee075f0f4f847a44
Stephen Jasina <stephenjasina@gmail.com>
2024-09-11 23:53:09 -0500

    Improve utilities for quickly plotting network graphs

--- src/plot_many_networks.py
+++ src/plot_many_networks.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 probes_path = '../data/animation_Europe/probes.csv'
-latencies_dir = '../data/animation_Europe/latencies_windowed_5/'
+latencies_dir = '../data/animation_Europe/latencies_median/'
 for latencies_filename in os.listdir(latencies_dir):
     latencies_path = os.path.join(latencies_dir, latencies_filename)
     subprocess.run([
@@ -12,5 +12,5 @@ for latencies_filename in os.listdir(latencies_dir):
         '-l', latencies_path,
         '-e', '5',
         '-m',
-        '-o', os.path.join('networks_windowed', os.path.splitext(latencies_filename)[0] + '.png')
+        '-o', os.path.join('networks_median', os.path.splitext(latencies_filename)[0] + '.png')
     ])
