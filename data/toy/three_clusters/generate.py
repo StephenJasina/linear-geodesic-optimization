8527189327f398133ab1506322264068d83cd1e6
Stephen Jasina <stephenjasina@gmail.com>
2024-11-18 06:06:49 -0600

    Add new small toy example with three clusters

--- data/toy/three_clusters/generate.py
+++ data/toy/three_clusters/generate.py
@@ -11,13 +11,13 @@ from linear_geodesic_optimization.data import curvature, utility
 
 
 graph = nx.Graph()
-n = 7
-cluster_radius = 0.125
+n = 6
+cluster_radius = 0.06
 
 # Generate the first cluster
-cluster_center = np.array([-0.25, 0.])
+cluster_center = np.array([1., 0.]) / 4
 for i in range(n):
-    xy = cluster_center + cluster_radius * np.array([np.cos(2 * np.pi * i / n), np.sin(2 * np.pi * i / n)])
+    xy = cluster_center + cluster_radius * np.array([np.sin(2 * np.pi * i / n), np.cos(2 * np.pi * i / n)])
     long, lat = utility.inverse_mercator(xy[0], xy[1])
     graph.add_node(
         f'a{i}',
@@ -34,9 +34,9 @@ for i in range(n - 1):
         )
 
 # Generate the second cluster
-cluster_center = np.array([0.25, 0.])
+cluster_center = np.array([-0.5, np.sqrt(3.) / 2.]) / 4.
 for i in range(n):
-    xy = cluster_center - cluster_radius * np.array([np.cos(2 * np.pi * i / n), np.sin(2 * np.pi * i / n)])
+    xy = cluster_center + cluster_radius * np.array([np.sin(2 * np.pi * i / n), np.cos(2 * np.pi * i / n)])
     long, lat = utility.inverse_mercator(xy[0], xy[1])
     graph.add_node(
         f'b{i}',
@@ -52,8 +52,28 @@ for i in range(n - 1):
             throughput = 1.
         )
 
+# Generate the third cluster
+cluster_center = np.array([-0.5, -np.sqrt(3.) / 2.]) / 4.
+for i in range(n):
+    xy = cluster_center + cluster_radius * np.array([np.sin(2 * np.pi * i / n), np.cos(2 * np.pi * i / n)])
+    long, lat = utility.inverse_mercator(xy[0], xy[1])
+    graph.add_node(
+        f'c{i}',
+        city = f'c{i}',
+        country = '',
+        lat = lat,
+        long = long
+    )
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        graph.add_edge(
+            f'c{i}', f'c{j}',
+            throughput = 1.
+        )
+
 # Join the clusters
-graph.add_edge('a0', 'b0')
+graph.add_edge('a5', 'b2', throughput = 1.)
+graph.add_edge('a4', 'c1', throughput = 1.)
 
 # Write the probes
 with open('probes.csv', 'w') as f:
@@ -78,9 +98,9 @@ with open('probes.csv', 'w') as f:
 # Write the throughputs and graphml
 os.makedirs('throughputs', exist_ok=True)
 os.makedirs('graphml', exist_ok=True)
-for index, center_throughput in enumerate(np.linspace(2., 0., 25)):
-    graph.edges['a0', 'b0']['throughput'] = float(center_throughput)
-
+# for index, center_throughput in enumerate(np.linspace(2., 0., 25)):
+#     graph.edges['a0', 'b0']['throughput'] = float(center_throughput)
+for index, _ in enumerate([None]):
     ricci_curvatures = curvature.ricci_curvature_optimal_transport(graph, edge_weight_label = 'throughput')
     for (source, target), ricci_curvature in ricci_curvatures.items():
         graph.edges[source, target]['ricciCurvature'] = ricci_curvature
