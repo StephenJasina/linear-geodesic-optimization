Fix bug that ignored final edge in distance computation
A typo (two swapped function calls) caused the final edge to be ignored
when computing the distance to the border.

src/linear_geodesic_optimization/graph/boundary.py
@@ -572,5 +572,5 @@ def distance_to_border(
     return min([
         np.linalg.norm(point - project_to_line_segment(point, left, right))
         for polygon in border
-        for left, right in itertools.islice(itertools.cycle(itertools.pairwise(polygon)), len(polygon))
+        for left, right in itertools.islice(itertools.pairwise(itertools.cycle(polygon)), len(polygon))
     ])