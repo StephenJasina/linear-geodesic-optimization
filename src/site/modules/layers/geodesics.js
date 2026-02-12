ab87ddc5f9052dda1d354b2fe0d6b7064a6191cf
Stephen Jasina <stephenjasina@gmail.com>
2026-02-12 09:08:30 -0600

    Ignore 0-length geodesics

--- src/site/modules/layers/geodesics.js
+++ src/site/modules/layers/geodesics.js
@@ -11,6 +11,9 @@ export default function(context, geodesics, geodesicsWidth = 3, colors = null) {
 
 	for (let index = 0; index < geodesics.length; ++index) {
 		let geodesic = geodesics[index];
+		if (geodesic.length == 0) {
+			continue;
+		}
 		let vertex = vertexToCanvasCoordinates(context, geodesic[0]);
 		context.beginPath();
 		context.moveTo(vertex[0], vertex[1]);
