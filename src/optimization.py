81ee12f511b84c5d53a41872e743439c192049d8
Stephen Jasina <stephenjasina@gmail.com>
2026-04-30 02:50:48 -0500

    Remove old optimization and collation files
    This is the final switch over to the "batched" versions of the files,
    which are configured via JSON files rather than changing source code.

--- src/optimization.py
+++ src/optimization.py
@@ -1,17 +1,16 @@
-import concurrent.futures
+import argparse
 import itertools
 import json
 import os
 import pathlib
 import shutil
-import sys
-import traceback
 import warnings
 
 import networkx as nx
 import numpy as np
 import scipy
 
+import linear_geodesic_optimization.batch as batch
 from linear_geodesic_optimization.data import input_network
 from linear_geodesic_optimization.mesh.rectangle import Mesh as RectangleMesh
 from linear_geodesic_optimization.optimization import optimization
@@ -20,27 +19,46 @@ from linear_geodesic_optimization.optimization import optimization
 # Error on things like division by 0
 warnings.simplefilter('error')
 
-directory_data = pathlib.PurePath('..', 'data')
+# List of parameters that correspond to extant files. This is used to
+# help generate output directory names (by removing extensions when they
+# would be unneeded) and determining actual locations of input files (by
+# prepending ../data)
+parameter_name_filenames = [
+    'filename_probes',
+    'filename_links',
+    'filename_graphml',
+    'filename_json',
+]
 
-def main(
+def argument_to_string(
+    arguments,
+    parameter_name
+):
+    if parameter_name in parameter_name_filenames:
+        return pathlib.PurePath(arguments[parameter_name]).stem
+    else:
+        return str(arguments[parameter_name])
+
+def optimize(
     *,  # All parameters are keyword only
-    filename_probes = None,
-    filename_links = None,
-    filename_graphml = None,
-    filename_json = None,
-    latency_threshold = None,
-    clustering_distance = None,
-    ricci_curvature_alpha = 0.9999,
-    lambda_curvature = 1.,
-    lambda_smooth = 0.,
+    filename_probes=None,
+    filename_links=None,
+    filename_graphml=None,
+    filename_json=None,
+    latency_threshold=None,
+    clustering_distance=None,
+    ricci_curvature_alpha=0.9999,
+    lambda_curvature=1.,
+    lambda_smooth=0.,
     initial_radius,
     sides,
-    mesh_scale = 1.,
-    coordinates_scale = 0.8,
-    network_trim_radius = None,
+    mesh_scale=1.,
+    coordinates_scale=0.8,
+    network_trim_radius=None,
     directory_output,
-    maxiter = None,
-    initialization_file_path = None,
+    maxiter=None,
+    initialization_file_path=None,
+    **kwargs
 ):
     # Construct the mesh
     width = height = sides
@@ -50,9 +68,9 @@ def main(
     routes = None
     traffic = None
     if filename_graphml is not None:
-        graph = nx.read_graphml(directory_data / filename_graphml)
+        graph = nx.read_graphml(filename_graphml)
     elif filename_json is not None:
-        file_path_json = directory_data / filename_json
+        file_path_json = filename_json
         graph, routes, traffic = input_network.get_graph_from_json(
             file_path_json,
             epsilon=latency_threshold,
@@ -60,8 +78,8 @@ def main(
             return_traffic=True,
         )
     elif filename_probes is not None and filename_links is not None:
-        file_path_probes = directory_data / filename_probes
-        file_path_links = directory_data / filename_links
+        file_path_probes = filename_probes
+        file_path_links = filename_links
         graph = input_network.get_graph_from_csvs(
             file_path_probes, file_path_links,
             epsilon=latency_threshold,
@@ -91,7 +109,6 @@ def main(
         'filename_graphml': str(filename_graphml) if filename_graphml is not None else None,
         'epsilon': float(latency_threshold) if latency_threshold is not None else None,
         'clustering_distance': float(clustering_distance) if clustering_distance is not None else None,
-        'should_remove_TIVs': False, # TODO: Pass this as a parameter?
         'ricci_curvature_alpha': float(ricci_curvature_alpha),
         'lambda_curvature': float(lambda_curvature),
         'lambda_smooth': float(lambda_smooth),
@@ -159,105 +176,126 @@ def main(
             'traffic': traffic,
         }, file_output, ensure_ascii=False, indent=4)
 
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_file')
+    args = parser.parse_args()
+    config_file = pathlib.PurePath(args.config_file)
+
+    # Read JSON config file and generate optimization parameters from it
+    defaults = {
+        'filename_probes': None,
+        'filename_links': None,
+        'filename_graphml': None,
+        'filename_json': None,
+        'latency_threshold': None,
+        'clustering_distance': None,
+        'ricci_curvature_alpha': 0.,
+        'lambda_curvature': 1.,
+        'lambda_smooth': 0.,
+        'initial_radius': 20.,
+        'sides': 50,
+        'mesh_scale': 1.,
+        'coordinates_scale': 0.8,
+        'network_trim_radius': None,
+        'maxiter': None,
+        'initialization_file_path': None,
+        'index': None # Additional unique ID
+    }
+    with open(config_file, 'r') as f:
+        arguments, settings = batch.parse_json(f, defaults)
+
+    # Determine where the outputs are going
+    if 'output_format' in settings:
+        output_format = settings['output_format']
+    else:
+        output_format = ['filename_json']
+    # Check that the output format has the right types
+    if not isinstance(output_format, list):
+        raise TypeError('Output format must be a list of (strings or lists of strings)')
+    for output_format_part in output_format:
+        if isinstance(output_format_part, str):
+            if output_format_part not in defaults:
+                raise ValueError(f'{output_format_part} not a valid part of output format')
+        elif isinstance(output_format_part, list):
+            for output_format_part_part in output_format_part:
+                if isinstance(output_format_part_part, str):
+                    if output_format_part_part not in defaults:
+                        raise ValueError(f'{output_format_part_part} not a valid part of output format')
+                else:
+                    raise TypeError('Output format must be a list of (strings or lists of strings)')
+        else:
+            raise TypeError('Output format must be a list of (strings or lists of strings)')
+    # Generate the output directories from the given format. Check if
+    # they already exist
+    for index, argument_dict in enumerate(arguments):
+        argument_dict['index'] = index
+        # Also prepend ../outputs (which is the path of the outputs
+        # directory relative to the script)
+        directory_output = pathlib.PurePath('..', 'outputs') / pathlib.PurePath(*(settings['directory_output'] + [
+            argument_to_string(argument_dict, output_format_part) if isinstance(output_format_part, str) else
+            '_'.join([argument_to_string(argument_dict, output_format_part_part) for output_format_part_part in output_format_part])
+            for output_format_part in output_format
+        ]))
+        if os.path.exists(directory_output):
+            raise ValueError(f'{str(directory_output)} already exists')
+        argument_dict['directory_output'] = directory_output
+    # Check whether the output directories overlap with themselves
+    # TODO: This can probably be improved by not constructing a set
+    if len(set(str(argument_dict['directory_output']) for argument_dict in arguments)) != len(arguments):
+        raise ValueError('Some output directories are duplicated')
+
+    # Optionally prepend a directory to where the input files are
+    # stored. This is to allow different choices of where the paths are
+    # relative to (the main data directory, the script's current
+    # directory, or the config file's directory (default))
+    directory_data_type = (
+        'config' if 'directory_data_type' not in settings else
+        settings['directory_data_type']
+    )
+    directory_data_parent = (
+        pathlib.PurePath('..', 'data') if directory_data_type == 'data' else
+        pathlib.PurePath() if directory_data_type == 'script' else
+        config_file.parent
+    )
+    directory_data = directory_data_parent / (pathlib.PurePath(*settings['directory_data']) if 'directory_data' in settings else '')
+    for argument_dict in arguments:
+        for parameter_name in parameter_name_filenames:
+            if argument_dict[parameter_name] is not None:
+                argument_dict[parameter_name] = directory_data / argument_dict[parameter_name]
+
+    if not arguments or 'dry_run' in settings and settings['dry_run']:
+        # Exit before producing output
+        return
+
+    # Determine the initialization strategy, and then use that to run
+    # the optimization, using multiprocessing where possible.
+    if 'initialization' in settings:
+        initialization = settings['initialization']
+    else:
+        initialization = 'sphere'
+    n_cores = settings['n_cores'] if 'n_cores' in settings else None
+    if initialization == 'sphere':
+        for argument_dict in arguments:
+            argument_dict['initialization_file_path'] = None
+        batch.run_multiprocessed(optimize, arguments, n_cores)
+    elif initialization == 'sequential':
+        # TODO: This behaves weirdly if the arguments don't form a
+        # single sequence (e.g., optimizing over a space of many input
+        # files and hyperparameter selections simultaneously)
+        arguments[0]['initialization_file_path'] = None
+        for argument_dict_previous, argument_dict in itertools.pairwise(arguments):
+            argument_dict['initialization_file_path'] = argument_dict_previous['directory_output'] / 'output.json'
+        batch.run_sequential(optimize, arguments)
+    elif initialization == 'first':
+        optimize(**arguments[0])
+        initializaiton_file_path = arguments[0]['directory_output'] / 'output.json'
+        for argument_dict in arguments[1:]:
+            argument_dict['initialization_file_path'] = initializaiton_file_path
+        batch.run_multiprocessed(optimize, arguments[1:], n_cores)
+    else:
+        raise ValueError(f'Invalid intiaization strategy "{initialization}"')
+
 if __name__ == '__main__':
-    n_cores = 14  # How many processes to use
-
-    # directory_outputs = pathlib.PurePath('..', 'outputs', 'Internet2', 'test')
-    # epsilon = None
-    # directory_json = pathlib.PurePath('Internet2', 'json')
-    # filenames_json = list(sorted(
-    #     filepath
-    #     for filename in sorted(os.listdir(directory_data / directory_json))
-    #     for filepath in (directory_json / filename,)
-    #     if filepath.suffix == '.json'
-    # ))[:25]
-    # count = len(filenames_json)
-    # filenames_probes = [None] * count
-    # filenames_links = [None] * count
-    # filenames_graphml = [None] * count
-
-    directory_outputs = pathlib.PurePath('..', 'outputs', 'toy', 'test_grid_size')
-    directory_json = pathlib.PurePath('toy', 'routing_with_volumes', 'graphs_single_route_change')
-    filenames_json = list(sorted(
-        filepath
-        for filename in sorted(os.listdir(directory_data / directory_json))
-        for filepath in (directory_json / filename,)
-        if filepath.suffix == '.json'
-    ))[:1] * 7
-    count = len(filenames_json)
-    filenames_probes = [None] * count
-    filenames_links = [None] * count
-    filenames_graphml = [None] * count
-
-    latency_thresholds = [None] * count
-    clustering_distances = [None] * count
-
-    lambdas_curvature = [1.] * count
-    lambdas_smooth = [0.002] * count
-    ricci_curvature_alphas = [0.] * count
-    # sides = [50] * count
-    sides = [20, 25, 30, 35, 40, 45, 50]
-    mesh_scales = [0.25] * count
-    initial_radii = [20. / mesh_scale for mesh_scale in mesh_scales]
-    coordinates_scales = [0.8] * count
-
-    network_trim_radii = [
-        # 0.2 * mesh_scale
-        None
-        for mesh_scale in mesh_scales
-    ]
-
-    directories_output = [
-        directory_outputs
-            / filename_json.stem
-            # / filename_links.stem
-            / f'{lambda_smooth}_{width}_{height}'
-        for index, (filename_probes, filename_links, filename_graphml, filename_json, lambda_smooth, latency_threshold, initial_radius, width, height, mesh_scale) \
-            in enumerate(zip(filenames_probes, filenames_links, filenames_graphml, filenames_json, lambdas_smooth, latency_thresholds, initial_radii, sides, sides, mesh_scales))
-    ]
-
-    maxiters = [None] * count
-
-    # Need to use ProcessPoolExecutor instead of multiprocessing.Pool
-    # to allow child processes to spawn their own subprocesses
-    with concurrent.futures.ProcessPoolExecutor(n_cores) as executor:
-        futures = []
-        for (
-            filename_probes, filename_links, filename_graphml, filename_json,
-            latency_threshold, clustering_distance, ricci_curvature_alpha,
-            lambda_curvature, lambda_smooth,
-            initial_radius, side, mesh_scale, coordinates_scale, network_trim_radius,
-            directory_output, maxiter
-        ) in zip(
-            filenames_probes, filenames_links, filenames_graphml, filenames_json,
-            latency_thresholds, clustering_distances, ricci_curvature_alphas,
-            lambdas_curvature, lambdas_smooth,
-            initial_radii, sides, mesh_scales, coordinates_scales, network_trim_radii,
-            directories_output, maxiters
-        ):
-            future = executor.submit(
-                main,
-                filename_probes = filename_probes,
-                filename_links = filename_links,
-                filename_graphml = filename_graphml,
-                filename_json = filename_json,
-                latency_threshold = latency_threshold,
-                clustering_distance = clustering_distance,
-                ricci_curvature_alpha = ricci_curvature_alpha,
-                lambda_curvature = lambda_curvature,
-                lambda_smooth = lambda_smooth,
-                initial_radius = initial_radius,
-                sides = side,
-                mesh_scale = mesh_scale,
-                coordinates_scale = coordinates_scale,
-                network_trim_radius = network_trim_radius,
-                directory_output = directory_output,
-                maxiter = maxiter
-            )
-            futures.append(future)
-        futures = concurrent.futures.wait(futures, return_when = 'FIRST_EXCEPTION')
-        for future in futures.done:
-            try:
-                future.result()
-            except Exception as exc:
-                print(traceback.format_exc())
+    main()
