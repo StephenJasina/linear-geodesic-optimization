d5ea8d56122b2ac144cb281eb7b1a51031db8297
Stephen Jasina <stephenjasina@gmail.com>
2026-05-11 03:27:37 -0500

    Update config file

--- src/plot_many_networks.py
+++ src/plot_many_networks.py
@@ -7,11 +7,15 @@ json_dir = data_dir / 'json'
 output_dir = data_dir / 'images'
 os.makedirs(output_dir, exist_ok=True)
 for json_filename in sorted(os.listdir(json_dir)):
+    if not '20260407060000.json' <= json_filename <= '20260408180000.json':
+        continue
     json_path = json_dir / json_filename
     name = f'{json_path.stem}.png'
     subprocess.run([
         'python',
         'plot_network.py',
         '-j', str(json_path),
+        '-e', '4',
+        '-c', '500000',
         '-o', str(output_dir / name)
     ])
